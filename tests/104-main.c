133da1bbc225916ac53a69f6001bd946a7761910
celacyleslie <celacydee@gmail.com>
2024-02-29 12:25:08 +0000

    BInary trees

---
+++ tests/104-main.c
@@ -0,0 +1,31 @@
+85% of storage used … If you run out, you can't create, edit, and upload files. Get 100 GB of storage for $2.79 $0.69/month for 3 months.
+#include <stdlib.h>
+#include <stdio.h>
+#include "../binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success, error code on failure
+ */
+int main(void)
+{
+    binary_tree_t *root;
+
+    root = binary_tree_node(NULL, 98);
+    root->left = binary_tree_node(root, 64);
+    root->left->left = binary_tree_node(root->left, 32);
+    binary_tree_print(root);
+    printf("Rotate-right %d\n", root->n);
+    root = binary_tree_rotate_right(root);
+    binary_tree_print(root);
+    printf("\n");
+
+    root->left->left = binary_tree_node(root->left, 20);
+    root->left->right = binary_tree_node(root->left, 56);
+    binary_tree_print(root);
+    printf("Rotate-right %d\n", root->n);
+    root = binary_tree_rotate_right(root);
+    binary_tree_print(root);
+    return (0);
+}
